edf7277bc65ee58b8c4da19b44fbe8eb5b784afa
VvsJaemin <djd453216@nate.com>
2022-08-25 23:05:54 +0900

    feat : 포인트컷 지시자 - execution, within, args

---
+++ src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,58 @@
+package hello.aop.pointcut;
+
+import hello.aop.member.MemberServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+public class WithinTest {
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        //public java.lang.String hello.aop.member.MemberServiceImpl.hello(java.lang.String)
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    // within 주의점 -> 표현식에 부모 타입을 지정하면 안된다는 점에서 execution과 차이가 난다.
+    @Test
+    void withinExact() {
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinStar() {
+        pointcut.setExpression("within(hello.aop.member.*Service*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void withinSubPackage() {
+        pointcut.setExpression("within(hello.aop..*)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    @DisplayName("타켓의 타입에만 직접 적용, 인터페이스를 신청하면 안된다.")
+    void withinSuperTypeFalse() {
+        pointcut.setExpression("within(hello.aop.member.MemberService)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    @DisplayName("execution은 타입 기반, 인터페이스 선정 가능.")
+    void executionSuperTypeTrue() {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+}
